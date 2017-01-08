62104e9ffb9f6ff1b58524f46e3a348c0d78cce2
yizhishi <283620911@qq.com>
2017-01-08 19:38:30 +0800

    初次提交

---
+++ src/main/java/gen/BaseMapper.java
@@ -0,0 +1,71 @@
+package gen;
+// package ${meta.mapperPackageName};
+//
+// import java.util.List;
+// import java.util.Map;
+//
+// import org.apache.ibatis.session.RowBounds;
+//
+// public interface BaseMapper<T> {
+//
+// /**
+// * 查询
+// */
+// List<T> find(T t);
+//
+// /*
+// * 分页查询
+// */
+// List<T> find(T entity, RowBounds rowBounds);
+//
+// /*
+// * 获取分页总条数
+// */
+// int count(T t);
+//
+// /**
+// * 查询
+// */
+// List<Map> findByMap(Map map);
+//
+// /*
+// * 分页查询
+// */
+// List<Map> findByMap(Map map, RowBounds rowBounds);
+//
+// /*
+// * 获取分页总条数
+// */
+// int countByMap(Map map);
+//
+// /**
+// * 查询单条记录
+// */
+// List<T> load(T t);
+//
+// /**
+// * 查询列表
+// */
+// List<T> loadByMap(Map map);
+//
+// /**
+// * 更新
+// */
+// int modify(T t);
+//
+// /**
+// * 更新
+// */
+// int modifyByMap(Map map);
+//
+// /**
+// * 新增
+// */
+// int add(T t);
+//
+// /**
+// * 新增
+// */
+// int addByMap(Map map);
+//
+// }
---
+++ src/main/java/gen/Main.java
@@ -0,0 +1,16 @@
+package gen;
+
+import generator.DatabaseOption;
+
+public class Main {
+
+	/*
+	 * 可以打jar包使用，放gen.properties和jar同一目录即可
+	 */
+	public static void main(String[] args) throws Exception {
+		DatabaseOption opt = new DatabaseOption();
+		opt.wirteFiles();
+		System.out.println("OK!");
+	}
+
+}
